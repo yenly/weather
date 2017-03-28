16ace683479dca811aabd9ba7cea358ca4332b8f
Yenly Ma <yencodes@gmail.com>
2017-03-28 16:32:49 -0700

    Add example jest text.

--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,13 @@ it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
 });
+
+// example jest test
+// use 'describe' to group together similar tests
+describe('Example Addition Test', () => {
+  // use 'it' to test a single attribute of a target
+  it('knows that 2 and 2 make 4', () => {
+    // use 'expect' to make an 'assertion' about a target
+    expect(2 + 2).toBe(4);
+  });
+});
