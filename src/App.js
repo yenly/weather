b93970b00ec43a8943e02cf34b73b9013295ec7e
Yenly Ma <yencodes@gmail.com>
2017-03-31 15:47:10 -0700

    Use constructor to grab browser's geolocation and store in state.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,30 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      lat: 90.0,
+      long: 0.0
+    }
+
+    // Checks if geolocation exist, then get lat and long to store in state.
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        this.setState({
+          lat: position.coords.latitude,
+          long: position.coords.longitude
+        });
+      });
+    } else {
+        console.log("Geolocation is not supported by this browser.");
+    }
+  }
+
+  componentDidMount() {
+
+  }
+
   render() {
     return (
       <div className="App">
@@ -11,7 +35,7 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Lat: {this.state.lat}, Long: {this.state.long}
         </p>
       </div>
     );
